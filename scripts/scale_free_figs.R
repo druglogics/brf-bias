4c2105a761653cc770f63347b123593671a2b325
john <bblodfon@gmail.com>
2021-01-06 16:33:39 +0100

    refine figures

--- scripts/scale_free_figs.R
+++ scripts/scale_free_figs.R
@@ -13,9 +13,9 @@ sf_topo_stats %>%
   geom_boxplot() +
   scale_fill_brewer(palette = "Set1") +
   scale_x_continuous(breaks = c(2,2.5)) +
-  guides(fill = guide_legend(title = "Gamma")) +
-  labs(y = "Number of equations with link operators", x = "Scale-free gamma parameter",
-    title = "Scale-free topologies - link-operator equations") +
+  guides(fill = guide_legend(title = latex2exp::TeX("$\\gamma$"))) +
+  labs(y = "Number of link operator nodes", x = latex2exp::TeX("Scale-free exponent $\\gamma$"),
+    title = "") +
   theme_classic(base_size = 14)
 ggsave(filename = "img/lo_eq_density.png", dpi = "print", width = 7, height = 5)
 
@@ -24,9 +24,9 @@ sf_topo_stats %>%
   geom_boxplot() +
   scale_fill_brewer(palette = "Set1") +
   scale_x_continuous(breaks = c(2,2.5)) +
-  guides(fill = guide_legend(title = "Gamma")) +
-  labs(y = "Maximum in-degree", x = "Scale-free gamma parameter",
-    title = "Scale-free topologies - #regulators of biggest hub node") +
+  guides(fill = guide_legend(title = latex2exp::TeX("$\\gamma$"))) +
+  labs(y = "Maximum in-degree", x = latex2exp::TeX("Scale-free exponent $\\gamma$"),
+    title = "") +
   theme_classic(base_size = 14)
 ggsave(filename = "img/max_reg_density.png", dpi = "print", width = 7, height = 5)
 
@@ -39,7 +39,7 @@ sf_topo_stats %>%
   scale_fill_brewer(palette = "Set1") +
   geom_jitter(shape = 20, position = position_jitter(0.2), show.legend = FALSE) +
   guides(fill = guide_legend(title = latex2exp::TeX("$\\gamma$"))) +
-  labs(x = "Scale-free gamma parameter", title = "Number of Stable states per abmlog-ensemble", y ="") + # for each abmlog-ensemble generated from a scale-free topology
+  labs(x = latex2exp::TeX("Scale-free exponent $\\gamma$"), title = "Number of Stable states per abmlog-ensemble", y ="") + # for each abmlog-ensemble generated from a scale-free topology
   theme_classic(base_size = 14) +
   theme(plot.title = element_text(hjust = 0.5))
 ggsave(filename = "img/ss_dist.png", dpi = "print", width = 7, height = 5)
@@ -47,7 +47,7 @@ ggsave(filename = "img/ss_dist.png", dpi = "print", width = 7, height = 5)
 # scale-free topologies percentage that every possible AND-NOT/OR-NOT link-operator configuration resulted in a zero stable state model
 sf_topo_stats %>%
   group_by(gamma) %>%
-  summarise(zero_ss_per = sum(ss_total == 0)/n()) %>%
+  summarise(zero_ss_per = sum(ss_total == 0)/n(), .groups = 'drop') %>%
   ggplot(aes(x = gamma, y = zero_ss_per, fill = as.factor(gamma))) +
   geom_col() +
   geom_text(aes(label = scales::percent(zero_ss_per)), vjust = -0.5, size = 10) +
@@ -55,7 +55,8 @@ sf_topo_stats %>%
   scale_y_continuous(labels = scales::percent, limits = c(0,1)) +
   scale_fill_brewer(palette = "Set1") +
   guides(fill = guide_legend(title = latex2exp::TeX("$\\gamma$"))) +
-  labs(x = "Scale-free gamma parameter", title = "Zero stable states for all abmlog models",
+  labs(x = latex2exp::TeX("Scale-free exponent $\\gamma$"),
+    title = "Zero stable states for all abmlog models",
     y = "Percentage of scale-free topologies") +
   theme_classic(base_size = 14) +
   theme(plot.title = element_text(hjust = 0.5))
@@ -63,7 +64,7 @@ ggsave(filename = "img/zero_ss_dist.png", dpi = "print", width = 7, height = 5)
 
 sf_topo_stats %>%
   group_by(gamma) %>%
-  summarise(sum_ss = sum(ss_total)) %>%
+  summarise(sum_ss = sum(ss_total), .groups = 'drop') %>%
     ggplot(aes(x = gamma, y = sum_ss, fill = as.factor(gamma))) +
     geom_col() +
     geom_text(aes(label = sum_ss), vjust = -0.5, size = 7) +
@@ -71,7 +72,8 @@ sf_topo_stats %>%
     scale_x_continuous(breaks = c(2,2.5)) +
     scale_fill_brewer(palette = "Set1") +
     guides(fill = guide_legend(title = latex2exp::TeX("$\\gamma$"))) +
-    labs(x = "Scale-free gamma parameter", title = "Total number of stable states", y = "") +
+    labs(x = latex2exp::TeX("Scale-free exponent $\\gamma$"),
+      title = "Total number of stable states", y = "") +
     theme_classic(base_size = 14) +
     theme(plot.title = element_text(hjust = 0.5))
 ggsave(filename = "img/ss_dist_total_sum.png", dpi = "print", width = 7, height = 5)
